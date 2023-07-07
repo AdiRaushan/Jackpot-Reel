944bcf7509ef5d3304975661e1abde764391feb0
AdiRaushan <adiraushan2k@gmail.com>
2023-07-07 22:31:11 +0530

    push

---
+++ pratice.js
@@ -0,0 +1,43 @@
+// deposit the money
+// number of lines to bet on
+//collect the bet money
+// spin the slot machine
+//if won declare it
+// give the user wining or lose take the money
+// play again
+
+const PromptSync = require("prompt-sync");
+
+// require function use to use prompt-sync
+const Prompt = require("prompt-sync")();
+
+const deposit = () => {
+  while (true) {
+    const depositNumber = Prompt("Enter the number:");
+    const IsNumberDeposit = parseFloat(depositNumber);
+
+    if (isNaN(IsNumberDeposit) || IsNumberDeposit <= 0) {
+      console.log("invalid Deposit Amount, try again.");
+    } else {
+      return IsNumberDeposit;
+    }
+  }
+};
+
+const GetNumberOfLines = () => {
+  while (true) {
+    const lines = Prompt("Enter number of lines:");
+    const numberOfLines = parseFloat(lines);
+
+    if (isNaN(numberOfLines) || numberOfLines <= 0 || numberOfLines > 3) {
+      console.log("Enter valid line you want to choose.");
+    } else {
+      return numberOfLines;
+    }
+  }
+};
+
+let balance = deposit();
+const numberOfLines = GetNumberOfLines();
+
+console.log(balance, numberOfLines);
